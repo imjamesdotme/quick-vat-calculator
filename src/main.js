5011eea605d2cec6ca0f003545794edb3d5abcda
James L <jameslilliott@me.com>
2024-09-16 20:08:41 +0100

    refactor(options): improve initialisation
    * move storage options logic to main file
    * format files
    * fix typos
    * update codekit config

--- src/main.js
+++ src/main.js
@@ -1,29 +1,28 @@
 function getValues() {
   return {
-    inital: document.getElementById('inital-value').value,
-    vat: document.getElementById('vat-percentage').value
+    initial: document.getElementById("initial-value").value,
+    vat: document.getElementById("vat-percentage").value,
   };
 }
 
-function setResult(processedInital, vatDiff, withVAT) {
-  document.getElementById('total-value').textContent = processedInital.toFixed(
-    2
-  );
-  document.getElementById('vat-value').textContent = vatDiff.toFixed(2);
-  document.getElementById('vat-type').textContent = withVAT ? 'Inc' : 'Ex';
+function setResult(processedInitial, vatDiff, withVAT) {
+  document.getElementById("total-value").textContent =
+    processedInitial.toFixed(2);
+  document.getElementById("vat-value").textContent = vatDiff.toFixed(2);
+  document.getElementById("vat-type").textContent = withVAT ? "Inc" : "Ex";
 }
 
 function addVAT() {
   var values = getValues();
-  var add = values.inital * (1 + values.vat / 100);
-  var vatDiff = add - values.inital;
+  var add = values.initial * (1 + values.vat / 100);
+  var vatDiff = add - values.initial;
   setResult(add, vatDiff, true);
 }
 
 function removeVAT() {
   var values = getValues();
-  var remove = values.inital / (1 + values.vat / 100);
-  var vatDiff = remove - values.inital;
+  var remove = values.initial / (1 + values.vat / 100);
+  var vatDiff = remove - values.initial;
   setResult(remove, vatDiff, false);
 }
 
@@ -46,35 +45,53 @@ function openOptions() {
   if (chrome.runtime.openOptionsPage) {
     chrome.runtime.openOptionsPage();
   } else {
-    window.open(chrome.runtime.getURL('options.html'));
+    window.open(chrome.runtime.getURL("options.html"));
   }
 }
 
 function initialiseOptions() {
-  chrome.storage.sync.get('options', function(storage) {
-    // If the value doesn't exist, default to 20.
-    document.getElementById('vat-percentage').value = storage.options
-      .vatPercentage
-      ? storage.options.vatPercentage
-      : 20;
+  chrome.storage.sync.get("options", function (storage) {
+    document.getElementById("vat-percentage").value =
+      storage.options.vatPercentage;
+    document.getElementById("currency").innerHTML = storage.options.currency;
+  });
+}
 
-    document.getElementById('currency').innerHTML = storage.options.currency;
+function setDefaultOptions() {
+  var defaultOptions = {
+    options: {
+      vatPercentage: 20,
+      currency: "&pound;",
+    },
+  };
+
+  chrome.storage.sync.set(defaultOptions, initialiseOptions);
+}
+
+function handleInitialseOptions() {
+  chrome.storage.sync.get("options", function (options) {
+    if (!options.hasOwnProperty("options")) {
+      setDefaultOptions();
+      return;
+    }
+
+    initialiseOptions();
   });
 }
 
 function initialise() {
-  // Focus on inital value input.
-  document.getElementById('inital-value').focus();
+  // Focus on initial value input.
+  document.getElementById("initial-value").focus();
   // Handle add/remove events.
-  document.getElementById('add').addEventListener('click', addVAT);
-  document.getElementById('remove').addEventListener('click', removeVAT);
+  document.getElementById("add").addEventListener("click", addVAT);
+  document.getElementById("remove").addEventListener("click", removeVAT);
   // Handle opening options.
-  document.getElementById('options').addEventListener('click', openOptions);
+  document.getElementById("options").addEventListener("click", openOptions);
   // Handle keydown events.
-  document.addEventListener('keydown', keyEventHandler, false);
+  document.addEventListener("keydown", keyEventHandler, false);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  initialiseOptions();
+document.addEventListener("DOMContentLoaded", function () {
+  handleInitialseOptions();
   initialise();
 });
--- src/options.js
+++ src/options.js
@@ -2,8 +2,8 @@ function handleSubmit(e) {
   e.preventDefault();
 
   var formValues = {
-    vatPercentage: document.getElementById('vat-percentage').value,
-    currency: document.getElementById('currency').value
+    vatPercentage: document.getElementById("vat-percentage").value,
+    currency: document.getElementById("currency").value,
   };
 
   if (validateVatInput(formValues.vatPercentage)) {
@@ -12,13 +12,13 @@ function handleSubmit(e) {
 }
 
 function validateVatInput(value) {
-  var errorMessage = document.querySelector('#vat-percentage ~ .error-message');
+  var errorMessage = document.querySelector("#vat-percentage ~ .error-message");
 
   if (!isNaN(value) && value >= 1) {
-    errorMessage.style.visibility = 'hidden';
+    errorMessage.style.visibility = "hidden";
     return true;
   } else {
-    errorMessage.style.visibility = 'visible';
+    errorMessage.style.visibility = "visible";
     return false;
   }
 }
@@ -26,52 +26,38 @@ function validateVatInput(value) {
 function saveOptions(formValues) {
   chrome.storage.sync.set(
     {
-      options: formValues
+      options: formValues,
     },
     function() {
-      var status = document.getElementById('status');
-      status.style.visibility = 'visible';
+      var status = document.getElementById("status");
+      status.style.visibility = "visible";
       setTimeout(function() {
-        status.style.visibility = 'hidden';
+        status.style.visibility = "hidden";
       }, 1500);
-    }
+    },
   );
 }
 
 function restoreOptions() {
-  chrome.storage.sync.get('options', function(storage) {
-    document.getElementById('vat-percentage').value =
+  chrome.storage.sync.get("options", function(storage) {
+    document.getElementById("vat-percentage").value =
       storage.options.vatPercentage;
-    document.getElementById('currency').value = storage.options.currency;
+    document.getElementById("currency").value = storage.options.currency;
   });
 }
 
-// Set default options & restore them into the interface.
-function initaliseOptions() {
-  var defaultOptions = {
-    options: {
-      vatPercentage: 20,
-      currency: '&pound;'
-    }
-  };
-
-  chrome.storage.sync.set(defaultOptions, restoreOptions);
-}
-
-function initalise() {
-  // Check if user has stored options & restore, otherwise initalise default options.
-  chrome.storage.sync.get('options', function(options) {
-    if (options.hasOwnProperty('options')) {
+function initialise() {
+  // Restore user options into the ui.
+  chrome.storage.sync.get("options", function(options) {
+    if (options.hasOwnProperty("options")) {
       restoreOptions();
       return;
     }
-
-    initaliseOptions();
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  initalise();
+document.addEventListener("DOMContentLoaded", function() {
+  initialise();
 
-  document.getElementById('save').addEventListener('click', handleSubmit);
+  document.getElementById("save").addEventListener("click", handleSubmit);
 });
